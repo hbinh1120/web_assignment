8ee0eb88d6345f03294bf76cfac60492fb9b9208
binh <binh.do1120@hcmut.edu.vn>
2021-11-25 19:22:07 +0700

    add image to product

--- api/addimagetoproduct.php
+++ api/addimagetoproduct.php
@@ -1,6 +1,6 @@
 <?php
     /*
-    delete product by id
+    add image url to product by id
     must be logged in as admin user
     */
     session_start();
@@ -9,13 +9,14 @@
         die();
     }
     $data = json_decode(file_get_contents('php://input'), true);
-    if (!empty($data) || !isset($data["product_id"])) {
+    if (!empty($data) || !isset($data["product_id"]) || !isset($data["imgurl"])) {
         header("HTTP/1.1 400 Bad Request");
         die();
     }
     include("database.php");
     $stmt = mysqli_stmt_init($dbc);
-    mysqli_stmt_prepare($stmt, "DELETE FROM product WHERE product_id=?");
+    mysqli_stmt_prepare($stmt, "INSERT INTO imgurl VALUES (?, ?)");
+    mysqli_stmt_bind_param($stmt, "s", $data["imgurl"]);
     mysqli_stmt_bind_param($stmt, "i", $data["product_id"]);
     mysqli_stmt_execute($stmt);
     if (mysqli_error($dbc) == "") {
--- api/modifyproductbyid.php
+++ api/modifyproductbyid.php
@@ -1,10 +1,15 @@
 <?php
     /*
     modify product by id, updates all specified parameters
+    must be logged in as admin user
     */
     session_start();
+    if (!isset($_SESSION["type"]) || $_SESSION["type"] != 1) {
+        header("HTTP/1.1 403 Forbidden");
+        die();
+    }
     $data = json_decode(file_get_contents('php://input'), true);
-    if (!empty($data) || !isset($data["id"])) {
+    if (!empty($data) || !isset($data["product_id"])) {
         header("HTTP/1.1 400 Bad Request");
         die();
     }
@@ -13,28 +18,28 @@
         $stmt = mysqli_stmt_init($dbc);
         mysqli_stmt_prepare($stmt, "UPDATE product SET product_name=? WHERE product_id=?");
         mysqli_stmt_bind_param($stmt, "s", $data["product_name"]);
-        mysqli_stmt_bind_param($stmt, "s", $data["id"]);
+        mysqli_stmt_bind_param($stmt, "i", $data["product_id"]);
         mysqli_stmt_execute($stmt);
     }
     if (isset($data["price"])) {
         $stmt = mysqli_stmt_init($dbc);
         mysqli_stmt_prepare($stmt, "UPDATE product SET price=? WHERE product_id=?");
         mysqli_stmt_bind_param($stmt, "i", $data["price"]);
-        mysqli_stmt_bind_param($stmt, "s", $data["id"]);
+        mysqli_stmt_bind_param($stmt, "i", $data["product_id"]);
         mysqli_stmt_execute($stmt);
     }
     if (isset($data["description"])) {
         $stmt = mysqli_stmt_init($dbc);
         mysqli_stmt_prepare($stmt, "UPDATE product SET description=? WHERE product_id=?");
         mysqli_stmt_bind_param($stmt, "s", $data["description"]);
-        mysqli_stmt_bind_param($stmt, "s", $data["id"]);
+        mysqli_stmt_bind_param($stmt, "i", $data["product_id"]);
         mysqli_stmt_execute($stmt);
     }
     if (isset($data["stock"])) {
         $stmt = mysqli_stmt_init($dbc);
         mysqli_stmt_prepare($stmt, "UPDATE product SET stock=? WHERE product_id=?");
         mysqli_stmt_bind_param($stmt, "i", $data["stock"]);
-        mysqli_stmt_bind_param($stmt, "s", $data["id"]);
+        mysqli_stmt_bind_param($stmt, "i", $data["product_id"]);
         mysqli_stmt_execute($stmt);
     }
     if (mysqli_error($dbc) == "") {
