87f60ba8a6ed39c4a3aaf2651cc85326b438ca05
binh <binh.do1120@hcmut.edu.vn>
2021-11-27 16:50:36 +0700

    get review list

---
+++ api/getreviewlist.php
@@ -0,0 +1,54 @@
+<?php
+    /*
+    returns json array
+    [
+        {
+            "username": string,
+            "product_id": int,
+            "rating": int,
+            "comment": string
+        },
+        ...
+    ]
+    finds by product if has parameter product_id, or by username if has parameter username
+    */
+    if (isset($_GET["product_id"])) {
+        include("database.php");
+        $stmt = mysqli_stmt_init($dbc);
+        $query = "
+            SELECT * FROM review WHERE product_id=?
+        ";
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_bind_param($stmt, "i", $_GET["product_id"]);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $response = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $response[] = $row;
+        }
+        echo json_encode($row);
+        header("HTTP/1.1 200 OK");
+        mysqli_close($dbc);
+        die();
+    }
+    if (isset($_GET["username"])) {
+        include("database.php");
+        $stmt = mysqli_stmt_init($dbc);
+        $query = "
+            SELECT * FROM review WHERE username=?
+        ";
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_bind_param($stmt, "i", $_GET["username"]);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $response = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $response[] = $row;
+        }
+        echo json_encode($row);
+        header("HTTP/1.1 200 OK");
+        mysqli_close($dbc);
+        die();
+    }
+    header("HTTP/1.1 400 Bad Request");
+?>
