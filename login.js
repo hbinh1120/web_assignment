83c722ccb4ee77e26c702a7c096d10247e854f68
dunglam2000vn <83953749+dunglam2000vn@users.noreply.github.com>
2021-11-27 21:10:43 +0700

    add login

--- login.js
+++ login.js
@@ -12,13 +12,13 @@ function login() {
         if (this.readyState == 4 && this.status == 200) {
             //see api/login.php for response format
             //do something
-            /*let status = JSON.parse(this.responseText);
-            console.log(this.responseText);
+            let status = JSON.parse(this.responseText);
+            //If you want back to login use this: window.location.href="http://localhost/web_assignment/product.php";
             if (status.status == 1)
                 window.location.replace("http://localhost/web_assignment/product.php");
             else {
-
-            }*/
+                document.getElementById("wronglogin").style.display = "inline";
+            }
         }
     };
     xmlhttp.open("POST", "api/login.php", true);
