4035051a4fbdcfb2dc48150353ec3f35f1097136
dunglam2000vn <83953749+dunglam2000vn@users.noreply.github.com>
2021-11-28 12:05:36 +0700

    add review for product UI

--- product.js
+++ product.js
@@ -106,6 +106,12 @@ function displayReviewList(reviewList) {
     reviewList.forEach(element => {
         let reviewcontainer = document.createElement("div");
         reviewcontainer.classList.add("reviewcontainer");
+        //LAD
+        let userimg = document.createElement("div");
+        userimg.classList.add("userimg");
+        let reviewinfo = document.createElement("div");
+        reviewinfo.classList.add("reviewinfo");
+        //LAD
         let reviewusername = document.createElement("div");
         reviewusername.classList.add("reviewusername");
         let reviewrating = document.createElement("div");
@@ -117,10 +123,25 @@ function displayReviewList(reviewList) {
         reviewrating.innerHTML = "Rating: " + element.rating;
         reviewcomment.innerHTML = element.comment;
 
+        //LAD
+        let uimg = document.createElement("img");
+        uimg.src = "img/user.png";
+        
+        userimg.appendChild(uimg);
+        reviewinfo.appendChild(reviewusername);
+        reviewinfo.appendChild(reviewrating);
+        reviewinfo.appendChild(reviewcomment);
+        reviewcontainer.appendChild(userimg);
+        reviewcontainer.appendChild(reviewinfo);
+        reviewlist.appendChild(reviewcontainer);
+        //LAD
+
+        /*
         reviewcontainer.appendChild(reviewusername);
         reviewcontainer.appendChild(reviewrating);
         reviewcontainer.appendChild(reviewcomment);
         reviewlist.appendChild(reviewcontainer);
+        */
     });
 }
 
