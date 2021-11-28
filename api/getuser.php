613c5de2b4a08d8b3d74100cdb7e1ef1a825d086
binh <binh.do1120@hcmut.edu.vn>
2021-11-28 15:20:19 +0700

    bug fix

--- api/getuser.php
+++ api/getuser.php
@@ -16,10 +16,10 @@
     include("database.php");
     $stmt = mysqli_stmt_init($dbc);
     $query = "
-        SELECT * FROM user WHERE username=?
+        SELECT * FROM user WHERE username LIKE ?
     ";
     mysqli_stmt_prepare($stmt, $query);
-    mysqli_stmt_bind_param($stmt, "i", $_GET["username"]);
+    mysqli_stmt_bind_param($stmt, "s", $_GET["username"]);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     while ($row = mysqli_fetch_assoc($result)) {
